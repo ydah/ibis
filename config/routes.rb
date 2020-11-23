c7837493a97ad0295dda375e1b41d006a746cb7b
Yudai Takada <t.yudai92@gmail.com>
2020-11-23 15:36:18 +0900

    Create home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
   get 'home/authentication'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
